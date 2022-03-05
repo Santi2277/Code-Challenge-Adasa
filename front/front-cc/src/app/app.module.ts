82bca36ffbf65dd5d5f29f520cb9a3e4d4f8c494
slozanocas <slozanocas@uoc.edu>
2022-03-05 18:33:09 +0100

    generated empty components

--- front/front-cc/src/app/app.module.ts
+++ front/front-cc/src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { GraphicComponent } from './components/graphic/graphic.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { MapComponent } from './components/map/map.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphicComponent,
+    HeaderComponent,
+    MainContentComponent,
+    MapComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
